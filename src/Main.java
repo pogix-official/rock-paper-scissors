e178a23c39e02878cecccf5abeb0bf05cac821fc
Pogix <simon.netusil@gmail.com>
2022-12-22 21:32:59 +0100

    gui basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("rock-paper-scissors");
+
+        frame.setSize(1000,1000);
+        frame.setVisible(true);
+
+        JButton startBtn = new JButton("Play");
+        frame.add(startBtn);
+        startBtn.setBounds(450,700,100,100);
+
+        startBtn.addActionListener(e -> {
+            startBtn.setVisible(false);
+
+            JButton paper = new JButton("paper");
+            JButton scissors = new JButton("scissors");
+            JButton rock = new JButton("rock");
+
+            frame.add(paper);
+            frame.add(scissors);
+            frame.add(rock);
+
+            paper.setBounds(200,700,100,100);
+            scissors.setBounds(400,700,100,100);
+            rock.setBounds(400,500,100,100);
+
+            paper.setVisible(true);
+            scissors.setVisible(true);
+            rock.setVisible(true);
+        });
     }
 }
